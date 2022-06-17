d5ce48e896753be1f3cbf9eba604ab89ed219941
Faraaz Ansari <faraaz.ansari@zeuslearning.com>
2022-06-17 15:43:11 +0530

    Categorize JSON data & add icons.json

--- Task-4/dashboard.js
+++ Task-4/dashboard.js
@@ -2,6 +2,10 @@ fetch('./data.json')
     .then(response => response.json())
     .then(result => fillData(result.data));
 
+// fetch('./icons.json')
+//     .then(response => response.json())
+//     .then(result => fillData(result.icons));
+
 const fillData = (courseData) => {
     const courseContainer = document.querySelector(".courses");
     courseData.forEach(item => {
@@ -10,24 +14,24 @@ const fillData = (courseData) => {
             `<div class="card">
                 <div class="card-header">
                     <div class="course-image">
-                        <img src="${item.cover}" alt="course 1">
+                        <img src="${item.heading.cover}" alt="course 1">
                     </div>
                     <div class="course-info">
                         <div class="title">
                             ${item.isExpired ? (`<span class="expired">EXPIRED</span>`) : (`<span></span>`)} 
-                            <h2>${item.title}</h2>
+                            <h2>${item.heading.title}</h2>
                             <img class=" ${item.favourite.isDisable ? 'disabled-star' : ''}" src="${item.favourite.icon}" alt="">
                         </div>
-                        <div class="grade-info"><span class="subject">${item.subject}</span><span class="line">|</span><span class="grade">${item.grade}</span>
-                            <span class="green-text">${item.greenText}</span>
+                        <div class="grade-info"><span class="subject">${item.class.subject}</span><span class="line">|</span><span class="grade">${item.class.grade}</span>
+                            <span class="green-text">${item.class.greenText}</span>
                         </div>
-                        <p class="syllabus"><span class="units bold-text">${item.units}</span> ${item.unitsText} <span class="lessons bold-text">${item.lessons}</span> ${item.lessonsText} <span class="topics bold-text">${item.topics}</span> ${item.topicsText}</p>
+                        <p class="syllabus"><span class="units bold-text">${item.syllabus.units}</span> ${item.syllabus.unitsText} <span class="lessons bold-text">${item.syllabus.lessons}</span> ${item.syllabus.lessonsText} <span class="topics bold-text">${item.syllabus.topics}</span> ${item.syllabus.topicsText}</p>
                         <div class="teacher">
                             <select id="teacher" name="teacher">
                             <option value="frank">${item.teacher}</option>
                             </select>
                         </div>
-                        <p class="students">${item.students} ${item.isDashVisible ? (`<span class="line">|</span>`) : (`<span></span>`)} <span class="date">${item.date}</span></p>
+                        <p class="students">${item.duration.students} ${item.duration.isDashVisible ? (`<span class="line">|</span>`) : (`<span></span>`)} <span class="date">${item.duration.date}</span></p>
                     </div>
                 </div>
                 <div class="options">
